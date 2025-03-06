c89b65a39af105228d2541fa36a1b295f7b34add
Alex Szefezuk <alexszefezuk@hotmail.com>
2025-03-06 15:57:03 -0300

    conversao de tipos com casting

---
+++ conversao-de-tipos/casting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10;
+  int b = 3;
+
+  float quociente = (float) a / b;
+  printf("Quociente: %.2f\n", quociente);
+
+  return 0;
+}
+
+/*
+  **Casting**
+  Aqui esta rolando uma conversão explicita de tipos de dados
+*/
