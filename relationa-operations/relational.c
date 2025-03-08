2c99b26fdfbecad095b7867031e72568516b77aa
Alex Szefezuk <alexszefezuk@hotmail.com>
2025-03-08 20:09:24 -0300

    comparações

---
+++ relationa-operations/relational.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+}
+
+/*
+  - > Maior
+  - < Menor
+  - == igual
+  - != diferente
+  - >= Maior ou igual
+  - <= menor ou igual
+
+
+  Aqui no c o retornos de comparações são 0 e 1
+
+
+  em comparações de tipos de dados diferentes o c faz uma conversão implicita
+*/
